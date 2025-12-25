6e89025257dab579446b007bc2f108b9db530173
Kieu Duc Tai <kdtai26022005@gmail.com>
2025-12-25 22:07:48 +0700

    Init HotelManagement: NetBeans + DBConnection OK

---
+++ src/main/Main.java
@@ -0,0 +1,34 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+//package main;
+//
+//import ui.MainFrame;
+//
+//public class Main {
+//    public static void main(String[] args) {
+//        new MainFrame().setVisible(true);
+//    }
+//}
+
+// test kết nối dtb
+//package main;
+//
+//import db.DBConnection;
+//
+//public class Main {
+//    public static void main(String[] args) {
+//        System.out.println(DBConnection.getConnection());
+//    }
+//}
+
+package main;
+
+import ui.MainFrame;
+
+public class Main {
+    public static void main(String[] args) {
+        new MainFrame().setVisible(true);
+    }
+}
